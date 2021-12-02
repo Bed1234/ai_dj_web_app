94f0556480d33e3d95d511d79737753251fab162
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-12-02 18:46:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
-
+scoreLW = 0;
+scoreRW = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -31,6 +32,9 @@ function gotPoses(results){
     if (results.length > 0) {
         console.log(results);
         
+        scoreLW = results[0].pose.keypoints[9].score;
+        scoreRW = results[0].pose.keypoints[10].score;
+        console.log("scoreLW = " + scoreLW + "scoreRW = " + scoreRW);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -38,7 +42,7 @@ function gotPoses(results){
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        console.log(" leftWristX = "+rightWristX +  " leftWristY = " + rightWristY);
+        console.log(" rightWristX = "+rightWristX +  " rightWristY = " + rightWristY);
 
         
 
@@ -54,6 +58,60 @@ function modelLoaded(){
 
 function draw(){
     image (video,0,0,600,500);
+
+
+    fill ("#FF0000");
+    stroke ("#FF0000");
+
+
+    if(scoreRW > 0.2){
+
+        circle(rightWristX , rightWristY , 20);
+
+        if (rightWristY > 0 && rightWristY <= 100) {
+            
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+    
+        } 
+        else if (rightWristY > 100 && rightWristY<= 200) {
+            document.getElementById("speed").innetHTML = "Speed = 1x";
+            song.rate(1);
+        }
+        
+        else if (rightWristY > 200 && rightWristY<= 300) {
+            document.getElementById("speed").innetHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+    
+        else if (rightWristY > 300 && rightWristY<= 400) {
+            document.getElementById("speed").innetHTML = "Speed = 2x";
+            song.rate(2);
+        }
+        else if (rightWristY > 400) {
+            document.getElementById("speed").innetHTML = "Speed = 2.5x";
+            song.rate(2.5);
+        }
+    }
+
+    
+
+
+ if (scoreLW > 0.2) {
+     
+    circle(leftWristX , leftWristY , 20);
+
+    lw_number = Number (leftWristY);
+
+    R_d = floor (lw_number);
+
+    volume = R_d / 500;
+
+    document.getElementById("volume").innerHTML = "volume = " + volume;
+
+    song.setVolume(volume);
+ }
+    
 }
 
 function play() {
